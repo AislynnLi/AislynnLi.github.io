37764df78203c955665a7064b68882f2357031b1
Aislynn <ql2103@nyu.edu>
2022-12-17 11:24:42 -0500

    Front Page

--- liu.js
+++ liu.js
@@ -1,13 +1,112 @@
-const theButton = document.querySelector("button")
-console.log(theButton)
+let week1_check = document.getElementById("week1_check")
+let week1 = document.getElementById("week1")
 
-let theMain = document.querySelector("body")
-let theTxt = document.getElementById("theText")
-
-theButton.addEventListener("click",changeColor)
-
-function changeColor(){
-    console.log("hi")
-    theTxt.textContent = "I want to change"
-    theMain.style.backgroundColor = "pink"
+function myFunction1(){
+if (week1_check.checked == true){
+    week1.style.display = "block"
 }
+}
+
+function myFunction2(){
+if (document.getElementById("week2_check").checked == true){
+    document.getElementById("week2").style.display = "block"
+}
+}
+
+function myFunction3(){
+if (document.getElementById("week3_check").checked == true){
+    document.getElementById("week3").style.display = "block"
+}
+}
+
+function myFunction4(){
+if (document.getElementById("week4_check").checked == true){
+    document.getElementById("week4").style.display = "block"
+}
+}
+
+function myFunction5(){
+if (document.getElementById("week5_check").checked == true){
+    document.getElementById("week5").style.display = "block"
+}
+}
+
+function myFunction6(){
+if (document.getElementById("week6_check").checked == true){
+    document.getElementById("week6").style.display = "block"
+}
+}
+
+function myFunction7(){
+if (document.getElementById("week7_check").checked == true){
+    document.getElementById("week7").style.display = "block"
+}
+}
+
+function myFunction8(){
+if (document.getElementById("week8_check").checked == true){
+    document.getElementById("week8").style.display = "block"
+}
+}
+
+function myFunction9(){
+if (document.getElementById("week9_check").checked == true){
+    document.getElementById("week9").style.display = "block"
+}
+}
+
+function myFunction10(){
+if (document.getElementById("week10_check").checked == true){
+    document.getElementById("week10").style.display = "block"
+}
+}
+
+function myFunction11(){
+if (document.getElementById("week11_check").checked == true){
+    document.getElementById("week11").style.display = "block"
+    document.getElementById("E1_check").checked = true
+    document.getElementById("E2_check").checked = true
+    document.getElementById("E3_check").checked = true
+    document.getElementById("E4_check").checked = true
+}
+}
+
+function myFunction12(){
+if (document.getElementById("week12_check").checked == true){
+    document.getElementById("week12").style.display = "block"
+    document.getElementById("W1_check").checked = true
+    document.getElementById("W2_check").checked = true
+    document.getElementById("W3_check").checked = true
+    document.getElementById("W4_check").checked = true
+    document.getElementById("W5_check").checked = true
+    document.getElementById("W6_check").checked = true
+    document.getElementById("W7_check").checked = true
+    document.getElementById("W8_check").checked = true
+}
+}
+
+function myFunctionW(){
+    document.getElementById("W1_check").checked = true
+    document.getElementById("W2_check").checked = true
+    document.getElementById("W3_check").checked = true
+    document.getElementById("W4_check").checked = true
+    document.getElementById("W5_check").checked = true
+    document.getElementById("W6_check").checked = true
+    document.getElementById("W7_check").checked = true
+    document.getElementById("W8_check").checked = true
+}
+
+function myFunctionE(){
+    document.getElementById("E1_check").checked = true
+    document.getElementById("E2_check").checked = true
+    document.getElementById("E3_check").checked = true
+    document.getElementById("E4_check").checked = true
+}
+
+
+
+
+
+
+
+alert("Hiii!!! ☀️Try to check the 'WEB' to see what I learned in this semester!")
